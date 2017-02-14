610659aee03e43951c2f864949a1188bbbf0a3a1
zhjc <735828661@qq.com>
2017-02-14 16:05:13 +0800

    初次提交

---
+++ algo/backtrace/BackTrace/BackTrace/config.h
@@ -0,0 +1,6 @@
+
+typedef enum
+{
+    AL_FALSE=0,
+    AL_TRUE
+} ALBOOL;
---
+++ algo/backtrace/BackTrace/BackTrace/nqueen.cpp
@@ -0,0 +1,11 @@
+
+// 经典 8-皇后问题 回溯解法
+
+#include "config.h"
+
+const int NUM_OF_QUEEN = 8;
+
+ALBOOL isPlace(int k)
+{
+    return AL_TRUE;
+}
