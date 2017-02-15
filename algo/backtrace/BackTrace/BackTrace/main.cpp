0141bc3a3720940f35b964a646c51006b78cf91b
zhjc <735828661@qq.com>
2017-02-15 16:40:28 +0800

    commit sum of subset solver

--- algo/backtrace/BackTrace/BackTrace/main.cpp
+++ algo/backtrace/BackTrace/BackTrace/main.cpp
@@ -4,9 +4,19 @@
 using namespace std;
 
 extern void nQueen();
+extern void SumSubSet(int s, int k, int r);
 
 int main()
 {
-    nQueen();
+    /*n-queen*/
+    //nQueen();
+
+    /*sum of subset*/
+    SumSubSet(0, 0, 73);
+    extern int flag;
+    cout << flag << endl;
+
+    /**/
+
     return 0;
 }
--- algo/backtrace/BackTrace/BackTrace/sumofsub.cpp
+++ algo/backtrace/BackTrace/BackTrace/sumofsub.cpp
@@ -4,7 +4,50 @@
 #include <cmath>
 #include <iostream>
 
-void SumSubSet()
-{
+using namespace std;
 
+int M = 30, n = 6; //M为总和，n为子集的元素个数
+int W[6] = { 5, 10, 12, 13, 15, 18 }; //集合
+bool X[6] = {}; //1为选择，0为不选择
+int flag = 0;
+
+void SumSubSet(int s, int k, int r)
+{
+    int sum = 0;
+    X[k] = 1;
+
+    if (s+W[k] == M)
+    {
+        flag++;
+
+        for (int i = 0; i < k+1; ++i)
+        {
+            cout << X[i] << " ";
+        }
+
+        cout << endl;
+
+        for (int i = 0; i < k+1; ++i)
+        {
+            if (X[i] == 1)
+            {
+                cout << W[i] << " ";
+                sum += X[i];
+            }
+        }
+
+        cout << endl;
+
+        sum = 0;
+    }
+    else if (s + W[k] + W[k+1] <= M)
+    {
+        SumSubSet(s + W[k], k + 1, r - W[k]);
+    }
+
+    if (s+r-W[k]>=M && s+W[k+1]<=M)
+    {
+        X[k] = 0;
+        SumSubSet(s, k + 1, r - W[k]);
+    }
 }
